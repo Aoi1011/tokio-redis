95bd86e54ea4add3d036fdf49369fe9c2cc8270a
Aoi1011 <ht.so.ik0102@gmail.com>
2023-03-03 21:30:41 +0900

    Store value

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+
+use mini_redis::{Connection, Frame, Command, cmd::{Set, Get}};
 use tokio::{net::{TcpListener, TcpStream}};
 
 #[tokio::main]
@@ -14,12 +16,27 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
+    let mut db = HashMap::new();
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            mini_redis::Command::Set(cmd) => {
+                // The value is stored as Vec<u8>
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            mini_redis::Command::Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented!: {:?}", cmd),
+        };
 
-        let response = Frame::Error("unimplemented".to_string());
+        // write the response to client
         connection.write_frame(&response).await.unwrap();
     }
 }
