9d6be6dcbc07fd6ca343fe230dc1c8df18b71ae1
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-21 10:12:06 +0900

    feat: implementing frame

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
 mod connection;
+
+pub mod frame;
+pub use frame::Frame;
+
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+
+/// A specialized `Result` type for mini-redis operations
+///
+/// This is defined as a convenience
+pub type Result<T> = std::result::Result<T, Error>;
+
