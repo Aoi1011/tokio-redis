304f10a7434bda0f09aaf87059e690dc1cf2fb18
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-22 18:51:33 +0900

    feat: implement command

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,7 +1,28 @@
-mod get; 
+mod get;
 pub use get::Get;
+
+mod unknown;
+pub use unknown::Unknown;
+
+use crate::{Frame, parse::Parse};
 
 #[derive(Debug)]
 pub enum Command {
     Get(Get),
+    Unknown(Unknown),
+}
+
+impl Command {
+    pub fn from_frame(frame: Frame) -> crate::Result<()> {
+        let mut parse = Parse::new(frame)?;
+
+        let command_name = parse.next_string()?.to_lowercase();
+
+        // let command = match command_name[..] {
+        //     "get" => Command::Get(Get::parse_frames(&mut parse)?),
+        //     _ => {
+        //     }
+        // }
+        Ok(())
+    }
 }
