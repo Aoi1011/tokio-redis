f2d203d97d552449ecf34e73d4c983d083ed29df
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-22 18:52:01 +0900

    feat: add unknown

---
+++ src/cmd/unknown.rs
@@ -0,0 +1,37 @@
+use tracing::debug;
+
+use crate::{Connection, Frame};
+
+/// Represents an "unknown" command. This is not a real `Redis` command.
+#[derive(Debug)]
+pub struct Unknown {
+    command_name: String,
+}
+
+impl Unknown {
+    /// Create a new `Unknown` command which responds to unknown commands
+    /// issued by clients
+    pub(crate) fn new(key: impl ToString) -> Unknown {
+        Unknown {
+            command_name: key.to_string(),
+        }
+    }
+
+    /// Returns the command name
+    pub(crate) fn get_name(&self) -> &str {
+        &self.command_name
+    }
+
+    /// Responds to the client, indicating the command is not recognized.
+    ///
+    /// This usually means the command is not yet implemented by `mini-redis`.
+    pub(crate) async fn apply(self, dst: &mut Connection) -> crate::Result<()> {
+        let res = Frame::Error(format!("ERR unknown command '{}'", self.command_name));
+
+        debug!(?res);
+
+        dst.write_frame(&res).await?;
+
+        Ok(())
+    }
+}
