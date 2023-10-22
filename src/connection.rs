ca1020fe1fcaaefc080d60760c3e8d1d61a2962c
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-22 16:14:20 +0900

    chore: add comment

--- src/connection.rs
+++ src/connection.rs
@@ -8,27 +8,72 @@ use tokio::{
 
 use crate::frame::{self, Frame};
 
+/// Send and receive `Frame` values from a remote peer.
+///
+/// When implementing networking protocols, a message on that protocol is
+/// often composed of several smaller messages known as frames. The purpose of
+/// `Connection` is to read and write frames on the underlying `TcpStream`.
+///
+/// To read frames, the `Connection` uses an internal buffer, which is filled
+/// up until there are enough bytes to create a full frame. Once this happens,
+/// the `Connection` creates the frame and returns it to the caller.
+///
+/// When sending frames, the frame is first encoded into the write buffer.
+/// The contents of the write buffer are then written to the socket.
 #[derive(Debug)]
 pub struct Connection {
+    // The `TcpStream`. It is decorated with a `BufWriter`. which provides write
+    // level buffering. The `BufWriter` implementation provided by Tokio is
+    // sufficient for our needs.
     stream: BufWriter<TcpStream>,
+
+    // The buffer for reading frames.
     buffer: BytesMut,
 }
 
 impl Connection {
+    /// Create a new `Connection`, backed by `socket`. Read and write buffers
+    /// are initialized.
     pub fn new(socket: TcpStream) -> Connection {
         Connection {
             stream: BufWriter::new(socket),
+            // Default to a 4KB read buffer. For the use case of mini redis,
+            // this is fine. However, real applications will want to tune this
+            // value to their specific use case. There is a high likelihood that
+            // a larger read buffer will work better.
             buffer: BytesMut::with_capacity(4 * 1024),
         }
     }
 
+    /// Read a single `Frame` value from the underlying stream.
+    ///
+    /// The function waits until it has retrieved enough data to parse a frame.
+    /// Any data reamining in the read buffer after the frame has been parsed is
+    /// kept there for the next call to `read_frame`.
+    ///
+    /// # Returns
+    ///
+    /// On success, the received frame is returned. If the `TcpStream`
+    /// is closed in a way that doesn't break in half, it returns
+    /// `None`. Otherwise, an error is returned.
     pub async fn read_frame(&mut self) -> crate::Result<Option<Frame>> {
         loop {
+            // Attempts to parse a frame from the buffered data. If enough data
+            // has been buffered, the frame is returned.
             if let Some(frame) = self.parse_frame()? {
                 return Ok(Some(frame));
             }
 
+            // There is not enough buffered data to read a frame. Attempt to
+            // read more data from the socket.
+            //
+            // On success, the number of bytes is returned. `0` indicates "end
+            // of stream".
             if self.stream.read_buf(&mut self.buffer).await? == 0 {
+                // The remote closed the connection. For this to be a clean 
+                // shutdown, there should be no data in the read buffer. If 
+                // there is, this means that the peer closed the socket while 
+                // sending a frame. 
                 if self.buffer.is_empty() {
                     return Ok(None);
                 } else {
