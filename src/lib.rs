1def646988ae82817024f1ac328e16b6e332cffc
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-22 19:42:53 +0900

    feat: create Shutdown and implement it

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,14 @@ mod parse;
 use parse::Parse;
 use parse::ParseError;
 
+mod shutdown;
+use shutdown::Shutdown;
+
+/// Default port that a redis server listens on. 
+///
+/// Used if no port is specified. 
+pub const DEFAULT_PORT: u16 = 6379;
+
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
 
 /// A specialized `Result` type for mini-redis operations
