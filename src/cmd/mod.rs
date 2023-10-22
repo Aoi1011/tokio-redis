028b2cad478afe4b2695eefe829d1445bcda8a78
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-22 19:29:15 +0900

    feat: from_frame

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -4,7 +4,7 @@ pub use get::Get;
 mod unknown;
 pub use unknown::Unknown;
 
-use crate::{Frame, parse::Parse};
+use crate::{parse::Parse, Frame, db::Db, Connection};
 
 #[derive(Debug)]
 pub enum Command {
@@ -13,16 +13,21 @@ pub enum Command {
 }
 
 impl Command {
-    pub fn from_frame(frame: Frame) -> crate::Result<()> {
+    pub fn from_frame(frame: Frame) -> crate::Result<Command> {
         let mut parse = Parse::new(frame)?;
 
         let command_name = parse.next_string()?.to_lowercase();
 
-        // let command = match command_name[..] {
-        //     "get" => Command::Get(Get::parse_frames(&mut parse)?),
-        //     _ => {
-        //     }
-        // }
-        Ok(())
+        let command = match &command_name[..] {
+            "get" => Command::Get(Get::parse_frames(&mut parse)?),
+            _ => {
+                return Ok(Command::Unknown(Unknown::new(command_name)));
+            }
+        };
+
+        parse.finish()?;
+
+        Ok(command)
     }
+
 }
