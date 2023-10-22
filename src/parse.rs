fa9317967968c06541d00c64fe5c57ea23fa2069
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-22 17:46:01 +0900

    chore: add comment in next_string

--- src/parse.rs
+++ src/parse.rs
@@ -4,18 +4,35 @@ use bytes::Bytes;
 
 use crate::Frame;
 
+/// Utility for parsing a command
+///
+/// Commands are represented as array frames. Each entry in the frame is a
+/// "token". A `Parse` is initialized with the array frame and provides a
+/// cursor-like API. Each command struct includes a `parse_frame` method that
+/// uses a `Parse` to extract its fields.
 #[derive(Debug)]
 pub(crate) struct Parse {
     parts: vec::IntoIter<Frame>,
 }
 
+/// Error encountered while parsing a frame.
+///
+/// Only `EndOfStream` errors handled at runtime. All other errors result in the connection being
+/// terminated.
 #[derive(Debug)]
 pub(crate) enum ParseError {
+    /// Attempting to extract a value failed due to the frame being fully
+    /// consumed.
     EndOfStream,
+
+    /// All other errors
     Other(crate::Error),
 }
 
 impl Parse {
+    /// Create a new `Parse` to parse the contents of `frame`.
+    ///
+    /// Returns `Err` if `frame` is not an array frame.
     pub(crate) fn new(frame: Frame) -> Result<Parse, ParseError> {
         let array = match frame {
             Frame::Array(array) => array,
@@ -27,12 +44,21 @@ impl Parse {
         })
     }
 
+    /// Return the next entry. Array frames are arrays of frames, so the next
+    /// entry is a frame.
     fn next(&mut self) -> Result<Frame, ParseError> {
         self.parts.next().ok_or(ParseError::EndOfStream)
     }
 
+    /// Return the next entry as a string.
+    ///
+    /// If the next entry cannot be represented as a String, then an error is returned.
     pub(crate) fn next_string(&mut self) -> Result<String, ParseError> {
         match self.next()? {
+            // Both `Simple` and `Bulk` representaion may be strings. Strings
+            // are parsed to UTF-8.
+            //
+            // While errors are stored as strings, they are considered separate types.
             Frame::Simple(s) => Ok(s),
             Frame::Bulk(data) => str::from_utf8(&data[..])
                 .map(|s| s.to_string())
