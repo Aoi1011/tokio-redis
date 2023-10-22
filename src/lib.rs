619fecd786baf5267ede834b31b309a9db07066f
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-22 10:45:36 +0900

    feat: define structs in db

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,13 @@ mod connection;
 pub mod frame;
 pub use frame::Frame;
 
+mod db;
+use db::Db;
+use db::DbDropGuard;
+
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
 
 /// A specialized `Result` type for mini-redis operations
 ///
 /// This is defined as a convenience
 pub type Result<T> = std::result::Result<T, Error>;
-
