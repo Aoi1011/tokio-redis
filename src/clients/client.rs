93caff1a31a8ab4de0da43bf0cd78a5ce7c318ac
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-28 07:21:52 +0900

    feat: impl Subscriber

--- src/clients/client.rs
+++ src/clients/client.rs
@@ -7,12 +7,14 @@ use std::{
     time::Duration,
 };
 
+use async_stream::try_stream;
 use bytes::Bytes;
 use tokio::net::{TcpStream, ToSocketAddrs};
+use tokio_stream::Stream;
 use tracing::{debug, instrument};
 
 use crate::{
-    cmd::{Get, Ping, Publish, Set, Subscribe},
+    cmd::{Get, Ping, Publish, Set, Subscribe, Unsubscribe},
     Connection, Frame,
 };
 
@@ -379,5 +381,116 @@ impl Client {
                 Err(err.into())
             }
         }
+    }
+}
+
+impl Subscriber {
+    /// Returns the set of channels currently subscribed to.
+    pub fn get_subscribed(&self) -> &[String] {
+        &self.subscribed_channels
+    }
+
+    /// Receive the next message published on a subscribed channel, waiting if necessary.
+    ///
+    /// `None` indicates the subscription has been terminated.
+    pub async fn next_message(&mut self) -> crate::Result<Option<Message>> {
+        match self.client.connection.read_frame().await? {
+            Some(mframe) => {
+                debug!(?mframe);
+
+                match mframe {
+                    Frame::Array(ref frame) => match frame.as_slice() {
+                        [message, channel, content] if *message == "message" => Ok(Some(Message {
+                            channel: channel.to_string(),
+                            content: Bytes::from(content.to_string()),
+                        })),
+                        _ => Err(mframe.to_error()),
+                    },
+                    _ => Err(mframe.to_error()),
+                }
+            }
+            None => Ok(None),
+        }
+    }
+
+    /// Convert the subscriber into a `Stream` yielding new messages published on subscribed
+    /// channels.
+    ///
+    /// `Subscriber` does not implement stream itself as doing so with safe code is non trivial.
+    /// The usage of async/await would require a manual Stream implementation to use `unsafe` code.
+    /// Instead, a conversion function is provided and the returned stream is implemented with the
+    /// help of the `async-stream` crate.
+    pub fn into_stream(mut self) -> impl Stream<Item = crate::Result<Message>> {
+        // Uses the `try_stream` macro from the `async-stream` crate. Generators are not stable in
+        // Rust. The crate uses a macro to simulate generators on top of async/await. There are
+        // limitations, so read the documentation there.
+        try_stream! {
+            while let Some(message) = self.next_message().await? {
+                yield message;
+            }
+        }
+    }
+
+    /// Subscribe to a list of new channels
+    #[instrument(skip(self))]
+    pub async fn subscribe(&mut self, channels: &[String]) -> crate::Result<()> {
+        // Issue the subscribe comamnd
+        self.client.subscribe_cmd(channels).await?;
+
+        // Update the set of subscribed channels.
+        self.subscribed_channels
+            .extend(channels.iter().map(Clone::clone));
+
+        Ok(())
+    }
+
+    /// Unsubscribe to a list of new channels
+    pub async fn unsubscribe(&mut self, channels: &[String]) -> crate::Result<()> {
+        let frame = Unsubscribe::new(channels).into_frame();
+
+        debug!(request = ?frame);
+
+        // Write the frame to the socket
+        self.client.connection.write_frame(&frame).await?;
+
+        // if the input channel list is empty, server acknowledge as unsubscribing from all
+        // subscribed channels, so we assert that the unsubscribe list received matches the client
+        // subscribed one
+        let num = if channels.is_empty() {
+            self.subscribed_channels.len()
+        } else {
+            channels.len()
+        };
+
+        // Read the response
+        for _ in 0..num {
+            let res = self.client.read_response().await?;
+
+            match res {
+                Frame::Array(ref frame) => match frame.as_slice() {
+                    [unsubscribe, channel, ..] if *unsubscribe == "unsubscribe" => {
+                        let len = self.subscribed_channels.len();
+
+                        if len == 0 {
+                            // There must be at least one channel
+                            return Err(res.to_error());
+                        }
+
+                        // unsubscribed channel should exist in the subscribed list at this point
+                        self.subscribed_channels.retain(|c| *channel != &c[..]);
+
+                        // Only a single channel should be removed from the list of subscribed
+                        // channels.
+                        if self.subscribed_channels.len() != len - 1 {
+                            return Err(res.to_error());
+                        }
+                    }
+                    _ => return Err(res.to_error()),
+                },
+                frame => return Err(frame.to_error()),
+            }
+        }
+
+        Ok(())
     }
 }
