3891747bd36d09279d8e413ae25efab63b055508
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-28 14:49:47 +0900

    feat: implement server_copy

---
+++ examples/echo_server_copy.rs
@@ -0,0 +1,30 @@
+use tokio::{
+    io::{self, AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let socket = TcpStream::connect("127.0.0.1:6142").await?;
+    let (mut rd, mut wr) = io::split(socket);
+
+    tokio::spawn(async move {
+        wr.write(b"hello\r\n").await?;
+        wr.write(b"world\r\n").await?;
+        Ok::<_, io::Error>(())
+    });
+
+    let mut buf = vec![0; 128];
+
+    loop {
+        let n = rd.read(&mut buf).await?;
+
+        if n == 0 {
+            break;
+        }
+
+        println!("GOT {:?}", n);
+    }
+
+    Ok(())
+}
