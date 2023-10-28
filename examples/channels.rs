034b0877db66498a481b7aebc2eac91edabef682
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-28 14:19:28 +0900

    feat: implement channel by oneshot

--- examples/channels.rs
+++ examples/channels.rs
@@ -1,12 +1,21 @@
 use bytes::Bytes;
 use mini_redis::clients::Client;
-use tokio::sync::mpsc;
+use tokio::sync::{mpsc, oneshot};
 
 #[derive(Debug)]
 enum Command {
-    Get { key: String },
-    Set { key: String, value: Bytes },
+    Get {
+        key: String,
+        resp: Responder<Option<Bytes>>,
+    },
+    Set {
+        key: String,
+        value: Bytes,
+        resp: Responder<()>,
+    },
 }
+
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
 #[tokio::main]
 async fn main() {
@@ -14,20 +23,33 @@ async fn main() {
     let tx2 = tx.clone();
 
     let t1 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
         let cmd = Command::Get {
             key: "foo".to_string(),
+            resp: resp_tx,
         };
 
+        // Send the GET request
         tx.send(cmd).await.unwrap();
+
+        // Await the response
+        let res = resp_rx.await;
+        println!("GOT = {:?}", res);
     });
 
     let t2 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
         let cmd = Command::Set {
             key: "foo".to_string(),
             value: "bar".into(),
+            resp: resp_tx,
         };
 
         tx2.send(cmd).await.unwrap();
+
+        let res = resp_rx.await;
+
+        println!("GOT = {:?}", res);
     });
 
     let manager = tokio::spawn(async move {
@@ -35,12 +57,13 @@ async fn main() {
 
         while let Some(cmd) = rx.recv().await {
             match cmd {
-                Command::Get { key } => {
-                    let val = client.get(&key).await.unwrap();
-                    println!("GOT = {:?}", val);
+                Command::Get { key, resp } => {
+                    let val = client.get(&key).await;
+                    let _ = resp.send(val);
                 }
-                Command::Set { key, value } => {
-                    client.set(&key, value).await.unwrap();
+                Command::Set { key, value, resp } => {
+                    let val = client.set(&key, value).await;
+                    let _ = resp.send(val);
                 }
             }
         }
