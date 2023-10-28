007a6672a4fe5c934488e570d72334104958b0eb
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-28 10:07:10 +0900

    feat: try spawning multiple threads

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,17 +1,33 @@
+use std::time::Duration;
+
 use mini_redis::{clients::Client, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // Open a connection to the mini-redis address.
-    let mut client = Client::connect("127.0.0.1:6379").await?;
 
-    // Set the key "hello" with value "world"
-    client.set("hello", "world".into()).await?;
+    for i in 0..10 {
+        tokio::spawn(async move {
+            let mut client = Client::connect("127.0.0.1:6379").await.unwrap();
+            // Set the key "hello" with value "world"
+            client
+                .set("hello", format!("world {}", i).into())
+                .await
+                .unwrap();
+        });
+    }
 
-    // Get key "hello"
-    let result = client.get("hello").await?;
+    for _ in 0..10 {
+        tokio::spawn(async {
+            let mut client2 = Client::connect("127.0.0.1:6379").await.unwrap();
+            // Get key "hello"
+            let result = client2.get("hello").await.unwrap();
 
-    println!("got value from the server; result = {:?}", result);
+            println!("got value from the server; result = {:?}", result);
+        });
+    }
+
+    tokio::time::sleep(Duration::from_secs(5)).await;
 
     Ok(())
 }
