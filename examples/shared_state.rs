b3c85933d2641b0ef8f538b6237c7bc3b681daa6
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-28 13:31:08 +0900

    feat: add shared_state examples

--- examples/shared_state.rs
+++ examples/shared_state.rs
@@ -1,37 +1,49 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 
+use bytes::Bytes;
 use mini_redis::{Command, Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening...");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         let (socket, _) = listener.accept().await.unwrap();
 
-        // A new task is spawned for each task inbound socket. The socket is moved to the new task
-        // and processed there.
+        let db = db.clone();
+
+        println!("Accepted");
+
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
-    let mut db = HashMap::new();
-
+async fn process(socket: TcpStream, db: Db) {
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let res = match Command::from_frame(frame).unwrap() {
             Command::Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Command::Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
@@ -41,4 +53,16 @@ async fn process(socket: TcpStream) {
 
         connection.write_frame(&res).await.unwrap();
     }
+}
+
+// fn new_shared_db(num_shards: usize) -> Db {
+//     let mut db = Vec::with_capacity(num_shards);
+//     for _ in 0..num_shards {
+//         db.push(Mutex::new(HashMap::new()));
+//     }
+//     Arc::new(db)
+// }
+async fn increment_and_do_stuff(mutex: &Mutex<i32>) {
+    let mut lock = mutex.lock().unwrap();
+    *lock += 1;
 }
